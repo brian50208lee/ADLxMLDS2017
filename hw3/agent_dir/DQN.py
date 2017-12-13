be2910dd4b6be9114d2d6b43a1b035c4277128af
brianlee <brian50208lee@gmail.com>
2017-12-14 00:45:34 +0800

    relu

--- hw3/agent_dir/DQN.py
+++ hw3/agent_dir/DQN.py
@@ -201,7 +201,7 @@ class DeepQNetwork(BasicDeepQNetwork):
         net = tf.layers.dense(
             inputs=net, 
             units=self.n_actions,
-            activation=None,
+            activation=tf.nn.relu,
             kernel_initializer=tf.contrib.layers.xavier_initializer(),
             name='fc5'
         )
