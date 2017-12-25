ce4c285921a0070e60f042649cd1041df144b049
brianlee <brian50208lee@gmail.com>
2017-12-25 23:30:41 +0800

    fix selu

--- hw4/GAN.py
+++ hw4/GAN.py
@@ -159,7 +159,7 @@ class GAN(BasicGAN):
     def leaky_relu(self, x, alpha=0.01):
         return tf.maximum(tf.minimum(0.0, alpha*x), x)
 
-    def selu(x):
+    def selu(self, x):
         alpha = 1.6732632423543772848170429916717
         scale = 1.0507009873554804934193349852946
         return scale*tf.where(x>=0.0, x, alpha*tf.nn.elu(x))
